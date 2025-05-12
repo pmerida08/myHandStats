a0604eb035822e5a3aaef2aa4617ed05fd71df94
caarlosbr <cborregueror@gmail.com>
2025-05-12 18:42:58 +0200

    vistas modificadas

--- frontend/src/views/Login.jsx
+++ frontend/src/views/Login.jsx
@@ -28,7 +28,7 @@ const Login = () => {
     e.preventDefault();
 
     try {
-      const res = await fetch("http://127.0.0.1:8000/usuarios/login", {
+      const res = await fetch("http://127.0.0.1:8000/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password }),
