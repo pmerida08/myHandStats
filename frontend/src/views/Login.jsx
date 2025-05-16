203553810ea9d1f54bc3be6058cc24134c0d8214
Pablo Mérida Velasco <a21mevepa@iesgrancapitan.org>
2025-05-16 13:02:11 +0200

    feat: actualizar URL de la API de inicio de sesión a producción

--- frontend/src/views/Login.jsx
+++ frontend/src/views/Login.jsx
@@ -28,7 +28,7 @@ const Login = () => {
     e.preventDefault();
 
     try {
-      const res = await fetch("http://127.0.0.1:8000/login", {
+      const res = await fetch("http://https://myhandstats.onrender.com/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password }),
