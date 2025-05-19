799deae7b37dbdf620e23e24bf5fbb61493b9dbc
Pablo Merida <pm3r1da@gmail.com>
2025-05-19 11:27:25 +0200

    Agregar origen permitido en CORS para la URL sin barra final

--- api/app/main.py
+++ api/app/main.py
@@ -10,7 +10,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "https://myhandstats.netlify.app/"  
+        "https://myhandstats.netlify.app/",
+        "https://myhandstats.netlify.app"  
     ],
     allow_credentials=True,
     allow_methods=["*"],
