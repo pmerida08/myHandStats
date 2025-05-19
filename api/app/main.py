7696de9b3364754690150d626c330055d53d2e3d
Pablo Merida <pm3r1da@gmail.com>
2025-05-19 08:59:07 +0200

    feat: corregir origen permitido para CORS en la API

--- api/app/main.py
+++ api/app/main.py
@@ -8,7 +8,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "https://myhandstats.onrender.com"],  
+    allow_origins=["http://localhost:5173", "https://myhandstats.netlify.app"],  
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
