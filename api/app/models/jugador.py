bea4bf97c27faeef475e214d2b88015d5da16f76
Jose M <jose.mayen@sepalo.es>
2025-05-22 18:42:11 +0200

    fix: Eliminar el campo 'posiciones' de la clase JugadorCreate

--- api/app/models/jugador.py
+++ api/app/models/jugador.py
@@ -54,7 +54,6 @@ class Jugador(BaseModel):
 class JugadorCreate(BaseModel):
     fecha_nac: date
     nombre: str
-    posiciones: List[int]
     foto: Optional[str] = None
     golesei: Optional[int] = 0
     golesli: Optional[int] = 0
