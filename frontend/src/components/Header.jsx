275166fafe9da77e26d1205f6c948edd2ac57d6d
Pablo Merida <pm3r1da@gmail.com>
2025-06-06 08:52:16 +0200

    feat: Eliminar referencias al Sidebar en el componente Header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,4 +1,3 @@
-import React, { useState } from "react";
 import { Flex, Icon, Text, Avatar } from "@chakra-ui/react";
 import { FaBars } from "react-icons/fa";
 /* import Sidebar from "./Sidebar";
@@ -10,13 +9,12 @@ const Header = ({ texto, club }) => {
     // Estado para controlar la apertura del Sidebar
 /*     const [isSidebarOpen, setSidebarOpen] = useState(false);
  */
-    const handleSidebarClose = () => setSidebarOpen(false);
 
     return (
         <>
             <Flex align="center" justify="space-between" mb={8}>
                 <Flex align="center" gap={3}>
-                    <Sidebar isOpen={isSidebarOpen} onClose={handleSidebarClose} />
+                    {/* <Sidebar isOpen={isSidebarOpen} onClose={handleSidebarClose} /> */}
                 </Flex>
                 <Flex align="center" gap={3} position="absolute" left="50%" transform="translateX(-50%)">
                     <Text fontSize="2xl" fontWeight="bold" color="#014C4C" mb={0}>
